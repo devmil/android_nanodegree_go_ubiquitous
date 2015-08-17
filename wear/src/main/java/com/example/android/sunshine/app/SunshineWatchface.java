99118b63d01db2bd861021fc45f1d56011bfe614
Devmil <develmil@gmail.com>
2015-08-17 23:22:59 +0200

    removed a ship full of unneeded things in the Watchface

--- wear/src/main/java/com/example/android/sunshine/app/SunshineWatchface.java
+++ wear/src/main/java/com/example/android/sunshine/app/SunshineWatchface.java
@@ -27,8 +27,6 @@ import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
 import android.view.Gravity;
@@ -39,13 +37,11 @@ import com.example.android.sunshine.app.events.WeatherDataUpdatedEvent;
 import com.example.android.sunshine.common.CommonUtils;
 import com.example.android.sunshine.common.WeatherDataTelegram;
 
-import java.lang.ref.WeakReference;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
-import java.util.concurrent.TimeUnit;
 
 import de.greenrobot.event.EventBus;
 
@@ -54,19 +50,6 @@ import de.greenrobot.event.EventBus;
  * low-bit ambient mode, the text is drawn without anti-aliasing in ambient mode.
  */
 public class SunshineWatchface extends CanvasWatchFaceService {
-    private static final Typeface NORMAL_TYPEFACE =
-            Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL);
-
-    /**
-     * Update rate in milliseconds for interactive mode. We update once a second since seconds are
-     * displayed in interactive mode.
-     */
-    private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(1);
-
-    /**
-     * Handler message id for updating the time periodically in interactive mode.
-     */
-    private static final int MSG_UPDATE_TIME = 0;
 
     @Override
     public Engine onCreateEngine() {
@@ -74,8 +57,6 @@ public class SunshineWatchface extends CanvasWatchFaceService {
     }
 
     private class Engine extends CanvasWatchFaceService.Engine {
-        final Handler mUpdateTimeHandler = new EngineHandler(this);
-
         final BroadcastReceiver mTimeZoneReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -125,22 +106,10 @@ public class SunshineWatchface extends CanvasWatchFaceService {
 
             setBackgroundColors(resources);
 
-            mTextPaintLightBig = new Paint();
-            mTextPaintLightBig.setTextAlign(Paint.Align.CENTER);
             mTextPaintLightBig = createTextPaint(resources.getColor(R.color.digital_text_light));
-            mTextPaintLightBig.setTypeface(Typeface.SANS_SERIF);
-            mTextPaintLightSmall = new Paint();
-            mTextPaintLightSmall.setTextAlign(Paint.Align.CENTER);
             mTextPaintLightSmall = createTextPaint(resources.getColor(R.color.digital_text_light));
-            mTextPaintLightSmall.setTypeface(Typeface.SANS_SERIF);
-            mTextPaintDarkBig = new Paint();
-            mTextPaintDarkBig.setTextAlign(Paint.Align.CENTER);
             mTextPaintDarkBig = createTextPaint(resources.getColor(R.color.digital_text_dark));
-            mTextPaintDarkBig.setTypeface(Typeface.SANS_SERIF);
-            mTextPaintDarkSmall = new Paint();
-            mTextPaintDarkSmall.setTextAlign(Paint.Align.CENTER);
             mTextPaintDarkSmall = createTextPaint(resources.getColor(R.color.digital_text_dark));
-            mTextPaintDarkSmall.setTypeface(Typeface.SANS_SERIF);
 
             mTime = GregorianCalendar.getInstance();
 
@@ -161,7 +130,6 @@ public class SunshineWatchface extends CanvasWatchFaceService {
         public void onDestroy() {
             EventBus.getDefault().unregister(this);
 
-            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME);
             super.onDestroy();
         }
 
@@ -174,7 +142,7 @@ public class SunshineWatchface extends CanvasWatchFaceService {
         private Paint createTextPaint(int textColor) {
             Paint paint = new Paint();
             paint.setColor(textColor);
-            paint.setTypeface(NORMAL_TYPEFACE);
+            paint.setTypeface(Typeface.SANS_SERIF);
             paint.setAntiAlias(true);
             return paint;
         }
@@ -191,10 +159,6 @@ public class SunshineWatchface extends CanvasWatchFaceService {
             } else {
                 unregisterReceiver();
             }
-
-            // Whether the timer should be running depends on whether we're visible (as well as
-            // whether we're in ambient mode), so we may need to start or stop the timer.
-            updateTimer();
         }
 
         private void registerReceiver() {
@@ -264,10 +228,6 @@ public class SunshineWatchface extends CanvasWatchFaceService {
                 setBackgroundColors(resources);
                 invalidate();
             }
-
-            // Whether the timer should be running depends on whether we're visible (as well as
-            // whether we're in ambient mode), so we may need to start or stop the timer.
-            updateTimer();
         }
 
         //instantiate it here even if it only gets used locally to avoid object instantiation in onDraw
@@ -312,38 +272,6 @@ public class SunshineWatchface extends CanvasWatchFaceService {
             drawDate(canvas, lMiddleStripeRect, lTimeTextBounds, dateText, lDateTextBounds);
             drawTemperatures(canvas, lLowerStripeRect, true);
             drawWeatherIcon(canvas, lLowerStripeRect);
-        }
-
-        /**
-         * Starts the {@link #mUpdateTimeHandler} timer if it should be running and isn't currently
-         * or stops it if it shouldn't be running but currently is.
-         */
-        private void updateTimer() {
-            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME);
-            if (shouldTimerBeRunning()) {
-                mUpdateTimeHandler.sendEmptyMessage(MSG_UPDATE_TIME);
-            }
-        }
-
-        /**
-         * Returns whether the {@link #mUpdateTimeHandler} timer should be running. The timer should
-         * only run when we're visible and in interactive mode.
-         */
-        private boolean shouldTimerBeRunning() {
-            return isVisible() && !isInAmbientMode();
-        }
-
-        /**
-         * Handle updating the time periodically in interactive mode.
-         */
-        private void handleUpdateTimeMessage() {
-            invalidate();
-            if (shouldTimerBeRunning()) {
-                long timeMs = System.currentTimeMillis();
-                long delayMs = INTERACTIVE_UPDATE_RATE_MS
-                        - (timeMs % INTERACTIVE_UPDATE_RATE_MS);
-                mUpdateTimeHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs);
-            }
         }
 
         private static final int TIME_DATE_MARGIN_PX = 10;
@@ -437,25 +365,5 @@ public class SunshineWatchface extends CanvasWatchFaceService {
             }
         }
 
-    }
-
-    private static class EngineHandler extends Handler {
-        private final WeakReference<SunshineWatchface.Engine> mWeakReference;
-
-        public EngineHandler(SunshineWatchface.Engine reference) {
-            mWeakReference = new WeakReference<>(reference);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            SunshineWatchface.Engine engine = mWeakReference.get();
-            if (engine != null) {
-                switch (msg.what) {
-                    case MSG_UPDATE_TIME:
-                        engine.handleUpdateTimeMessage();
-                        break;
-                }
-            }
-        }
     }
 }
